53950c9ee8e82780fd896ba3654106723a32b4fc
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2016-01-17 16:41:11 +1100

    fix bower include order in theme-previewer

--- gulp/styles.js
+++ gulp/styles.js
@@ -10,23 +10,37 @@ var $ = require('gulp-load-plugins')();
 
 var wiredep = require('wiredep').stream;
 var _ = require('lodash');
+var rename = require("gulp-rename");
 
 // Concatenate all LESS files in one
 gulp.task('less-concat', function() {
-  // The live-previewer file is used for published changes
-  // The live-previewer-tmp file is used for pending changes
-  return gulp.src([
+  var injectFiles = gulp.src([
     path.join(conf.paths.src, '/app/stylesheets/theme.less'),
     path.join(conf.paths.src, '/app/stylesheets/variables.less'),
     path.join(conf.paths.src, '/app/stylesheets/live-previewer.less'),
     path.join(conf.paths.src, '/app/stylesheets/live-previewer-tmp.less'),
-    path.join(conf.paths.src, '/app/**/*.less')
+    path.join(conf.paths.src, '/app/**/*.less'),
+    path.join('!' + conf.paths.src, '/app/index.less'),
   ])
-  .pipe($.concat('app.less'))
-  .pipe(wiredep(_.extend({}, conf.wiredep)))
-  .pipe($.replace('../../../bower_components/', '../bower_components/'))
-  .pipe($.replace('../../bower_components/', '../bower_components/'))
-  .pipe(gulp.dest(path.join(conf.paths.dist, '/styles/')))
+
+  var injectOptions = {
+    transform: function(filePath,file) {
+      return file.contents.toString('utf8')
+    },
+    starttag: '// injector',
+    endtag: '// endinjector',
+    addRootSlash: false
+  };
+
+  return gulp.src([
+    path.join(conf.paths.src, '/app/index.less')
+  ])
+    .pipe($.inject(injectFiles, injectOptions))
+    .pipe(wiredep(_.extend({}, conf.wiredep)))
+    .pipe($.replace('../../../bower_components/', '../bower_components/'))
+    .pipe($.replace('../../bower_components/', '../bower_components/'))
+    .pipe(rename({ basename: "theme-previewer" }))
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/styles/')))
 });
 
 gulp.task('styles', function () {
