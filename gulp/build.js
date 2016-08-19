ab1442c2f105f6101071d7b842335b45e986c71e
Xaun Lopez <xaunlopez@gmail.com>
2016-08-19 17:14:38 +1000

    Add gulp images task, configure impac-angular default images

--- gulp/build.js
+++ gulp/build.js
@@ -80,6 +80,13 @@ gulp.task('fonts', function () {
     .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
 });
 
+gulp.task('images', function () {
+  return gulp.src($.mainBowerFiles())
+    .pipe($.filter('**/*.{png,jpg,jpeg,gif}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/images/')));
+});
+
 gulp.task('other', function () {
   var fileFilter = $.filter(function (file) {
     return file.stat.isFile();
@@ -97,4 +104,4 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('build', ['html', 'fonts', 'other', 'images']);
