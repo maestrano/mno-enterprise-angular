e1ce9715a592781d2344ccfcade9acc833879455
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2017-07-11 15:05:00 +1000

    [MNOE-541] Theme Previewer: Fix advanced vars not working

--- gulp_tasks/styles.js
+++ gulp_tasks/styles.js
@@ -75,8 +75,8 @@ function sprites() {
 function lessConcat() {
   var injectFiles = gulp.src([
     conf.path.src('/app/stylesheets/theme.less'),
-    conf.path.src('/app/stylesheets/variables.less'),
     conf.path.src('/app/stylesheets/*.less'),
+    conf.path.src('/app/stylesheets/variables.less'),
     conf.path.src('/app/**/*.less'),
     conf.path.src('/fonts/**/*.less'),
     conf.path.src('/images/**/*.less'),
