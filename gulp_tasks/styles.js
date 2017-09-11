d91aaaf016fbd9e3a287ebf96b00705b5e73808a
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2017-09-11 15:06:45 +1000

    Fix theme previewer less compilation

--- gulp_tasks/styles.js
+++ gulp_tasks/styles.js
@@ -29,8 +29,9 @@ function styles() {
 
   var injectFiles = gulp.src([
     conf.path.src('/app/stylesheets/theme.less'),
+    conf.path.src('/app/stylesheets/variables-*.less'),
+    conf.path.src('/app/stylesheets/variables.less'),
     conf.path.src('/app/stylesheets/*.less'),
-    conf.path.src('/app/stylesheets/variables*.less'),
     conf.path.src('/app/**/*.less'),
     conf.path.src('/fonts/**/*.less'),
     conf.path.src('/images/**/*.less'),
@@ -75,8 +76,9 @@ function sprites() {
 function lessConcat() {
   var injectFiles = gulp.src([
     conf.path.src('/app/stylesheets/theme.less'),
-    conf.path.src('/app/stylesheets/*.less'),
+    conf.path.src('/app/stylesheets/variables-*.less'),
     conf.path.src('/app/stylesheets/variables.less'),
+    conf.path.src('/app/stylesheets/*.less'),
     conf.path.src('/app/**/*.less'),
     conf.path.src('/fonts/**/*.less'),
     conf.path.src('/images/**/*.less'),
